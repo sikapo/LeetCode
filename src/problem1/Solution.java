e6027e8b808fd8ee5c31321a6687ce1e558a94d2
Owner <vitycity79@gmail.com>
2023-03-07 12:36:19 +0200

    Innit commit

---
+++ src/problem1/Solution.java
@@ -0,0 +1,19 @@
+package problem1;
+
+public class Solution {
+        public boolean isPalindrome(int x) {
+            float xx = x;
+            int [] a = a;
+            if (xx <= 0) {
+                return false;
+            }
+            for (int i = 0; i < x.lenght; i++) {
+                if ((xx / 10) > 0) {
+                    a [i] = xx % 10;
+                    xx
+                }
+            }
+            for ()
+        }
+    }
+}
